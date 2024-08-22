aee8a41e55f034341e4cd158e6345621a774b129
Benjamin Schneider <ich@benjamin-schneider.com>
2024-08-22 13:31:23 +0200

    Add puppet

--- puppet.tf
+++ puppet.tf
@@ -8,38 +8,54 @@
 #                                    #
 ######################################
 
-# Filename: volunteersystem.tf
-# Description: Volunteer system configuration 
+# Filename: puppet.tf
+# Description: Puppet server configuration
 # Version: 1.0.0
 # Author: Benjamin Schneider <ich@benjamin-schneider.com>
-# Date: 2024-08-03
-# Last Modified: 2024-08-03
+# Date: 2024-08-20
+# Last Modified: 2024-08-20
 # Changelog: 
 # 1.0.0 - Initial version
 
-module "volunteersystem" {
+module "puppet" {
   source        = "gitlab.com/Javik/terraform-hcloud-modules/vserver"
   version       = "> 1.0.0"
   service_count = 1
 
-  name_prefix = "volunteersystem"
+  name_prefix = "puppet"
   domain      = module.globals.domain
+  type        = "cx22"
   environment = module.globals.environment
   network_id  = module.network.network.id
   ssh_key_ids = module.globals.ssh_key_ids
-  subnet      = "10.0.50.0/24"
-
-  labels = {
-    "managed_by" = "terraform"
-  }
+  subnet      = "10.0.70.0/24"
 
   additional_names = {
-    "demo.volunteers.events"      = {},
-    "demo.volunteer.rocks"        = {},
-    "demo.volunteering.solutions" = {},
+    "puppet.sirjavik.de" = {
+      proxy = false
+    },
+    "puppet.infra.sirjavik.de" = {
+      proxy = false
+    },
+  }
+
+  labels = {
+    "managed_by"   = "terraform",
+    "service_type" = "puppet",
+    "ha"           = false,
+    "ha_group"     = "puppet"
   }
 
   firewall_rules = [
+    {
+      direction   = "in"
+      protocol    = "icmp"
+      description = "icmp"
+      source_ips = [
+        "0.0.0.0/0",
+        "::/0"
+      ]
+    },
     {
       direction   = "in"
       protocol    = "tcp"
@@ -50,51 +66,12 @@ module "volunteersystem" {
         "::/0"
       ]
     },
-    {
-      direction   = "in"
-      protocol    = "tcp"
-      port        = "80"
-      description = "HTTP"
-      source_ips = [
-        "0.0.0.0/0",
-        "::/0"
-      ]
-    },
-    {
-      direction   = "in"
-      protocol    = "tcp"
-      port        = "443"
-      description = "HTTPS"
-      source_ips = [
-        "0.0.0.0/0",
-        "::/0"
-      ]
-    },
-    {
-      direction   = "in"
-      protocol    = "udp"
-      port        = "60000-61000"
-      description = "Mosh"
-      source_ips = [
-        "0.0.0.0/0",
-        "::/0"
-      ]
-    }
   ]
 
-  volumes = {
-    "wwwdata" = {
-      size = 10
-    },
-    "mysqldata" = {
-      size = 10
-    }
-  }
+  cloudflare_zones = module.globals.cloudflare_zones
 
   depends_on = [
     module.globals,
     module.network,
   ]
-
-  cloudflare_zones = module.globals.cloudflare_zones
 }
