be1b4723e7b35eb7711a809113cca9e7aac9bad2
Benjamin Schneider <ich@benjamin-schneider.com>
2025-04-07 20:52:43 +0200

    fmt

--- mailserver.tf
+++ mailserver.tf
@@ -30,8 +30,8 @@ module "mail" {
   ssh_key_ids = module.globals.ssh_key_ids
   subnet      = "10.0.40.0/24"
 
-  backups     = true
-  protected   = true
+  backups   = true
+  protected = true
 
   labels = {
     "managed_by"   = "terraform",
